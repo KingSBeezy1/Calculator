00f28903ec2ed200a02ec7026873036ac4fa28f5
KingSBeezy <jewsslainjesus@gmail.com>
2022-01-19 11:06:47 +0200

    calc

--- OptionsManager.cs
+++ OptionsManager.cs
@@ -5,14 +5,29 @@ namespace GitExercise
     public static class OptionsManager
     {
         public static string[] OptionsList = {
-            "a - Add"
+            "a - Add",
+            "s - substract",
+            "m - multiply",
+            "d - divide"
         };
 
         public static void Add(double a, double b)
         {
             Console.WriteLine($"{a} + {b} = {a + b}");
         }
+        public static void Substract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} * {b} = {a * b}");
+        }
+        public static void Divide(double a, double b)
+        {
+            Console.WriteLine($"{a} / {b} = {a / b}");
+        }
 
-       
+
     }
 }
